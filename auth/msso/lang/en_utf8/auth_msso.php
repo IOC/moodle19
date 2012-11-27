87e1aa613800925dc556479e760f58c43eb81146
Albert Gasset <albert.gasset@gmail.com>
2012-11-27 09:50:44 +0100

    SSO auth plugin

---
+++ auth/msso/lang/en_utf8/auth_msso.php
@@ -0,0 +1,15 @@
+<?php
+
+$string['auth_mssodescription'] = 'Simple signle sign-on authentication for Moodle';
+$string['auth_mssotitle'] = 'Moodle SSO';
+$string['config_idp'] = 'Identity provider URL';
+$string['config_key'] = 'Secret key';
+$string['config_sp'] = 'Service provider URL';
+$string['config_ttl'] = 'TTL of requests';
+$string['error_configidp'] = 'SSO: identity provider not configured';
+$string['error_configkey'] = 'SSO: secret key not configured';
+$string['error_configsp'] = 'SSO: service provider not configured';
+$string['error_expiredrequest'] = 'SSO: expired request';
+$string['error_invalidhash'] = 'SSO: invalid hash';
+$string['error_invalidrequest'] = 'SSO: invalid request';
+$string['error_invaliduser'] = 'SSO: invalid user';
